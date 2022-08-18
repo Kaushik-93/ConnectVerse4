1e13abb9be50074b42775a7f30f87565b4d8d123
Kaushik-93 <k24561323@gmail.com>
2022-08-18 10:12:11 +0530

    Initil Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./components/Home/NavBar";
+import Stats from "./components/Home/Stats";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Stats.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Stats.css";
+const Stats = () => {
+  return (
+    <div className="stats-container">
+      <div className="stats">
+        <div className="div-village">
+          <h3 className="stats-no">640K</h3>
+          <p className="stats-content">Villages in India</p>
+        </div>
+        <div className="div-unemployment">
+          <h3 className="stats-no">640K</h3>
+          <p className="stats-content">Villages in India</p>
+        </div>
+        <div className="div-population">
+          <h3 className="stats-no">640K</h3>
+          <p className="stats-content">Villages in India</p>
+        </div>
+        <div className="div-no-of-unemployment">
+          <h3 className="stats-no">640K</h3>
+          <p className="stats-content">Villages in India</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Stats;
